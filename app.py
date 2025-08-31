053b480759df38c659728bc944286e92d4837066
heidarosmalldoll80 <riyensarahh@hotmail.com>
2025-08-31 03:42:27 -0500

    Add comments for better clarity and fix image saving path

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ def index():
     if request.method == 'POST':
         file = request.files['image']
         if file:
-            # Bild lesen
+            # Bild speichern
             img_path = './uploads/' + file.filename
             file.save(img_path)
 
-            # Gesichtserkennung
+            # Gesichtserkennung aufgerufen
             face_detection(img_path)
             return redirect(url_for('results', filename=file.filename))
     return render_template('index.html')
@@ -27,16 +27,16 @@ def face_detection(img_path):
     # Lade das Haar-Cascade-Klassifikator
     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-    # Bild laden
+    # Bild laden und in Graustufen umwandeln
     img = cv2.imread(img_path)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
 
-    # Gesichter zeichnen
+    # Gesichter in das Bild zeichnen
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
 
-    # Speichern des Bearbeiteten Bildes
+    # Speichern des bearbeiteten Bildes im statischen Verzeichnis
     cv2.imwrite('./static/result_' + img_path.split('/')[-1], img)
 
 
